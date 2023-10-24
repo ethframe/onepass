cc202b12aec981a12cbebea36c4f44333425fb49
ethframe <ethframe@gmail.com>
2023-10-24 22:54:53 +0300

    Move parts of code emitting into Stack

--- onepass.py
+++ onepass.py
@@ -154,7 +154,7 @@ class ExprTranslator(ExprVisitor):
         stack = self._stack.scope()
         slots: list[int] = []
         for _ in range(max(0, len(expr.args) - len(self._cc.registers))):
-            slots.append(stack.allocate())
+            slots.append(stack.allocate(self._emitter))
         regs: list[str] = []
         for i, arg in enumerate(expr.args):
             arg.accept(self)
@@ -165,64 +165,57 @@ class ExprTranslator(ExprVisitor):
                 regs.append(reg)
         while regs:
             self._emitter.emit(f"popq    {regs.pop()}")
-        adjust = (8 * len(self._cc.registers) if self._cc.shadow else 0) + \
-            self._stack.align(self._cc.alignment)
-        if adjust > 0:
-            self._emitter.emit(f"subq    ${adjust}, %rsp")
+        stack.adjust(self._cc, self._emitter)
         self._emitter.emit(f"call    {expr.name}")
-        if adjust > 0:
-            self._emitter.emit(f"addq    ${adjust}, %rsp")
+        stack.free(self._emitter)
         self._emitter.emit("pushq   %rax")
-        allocated = stack.allocated()
-        if allocated > 0:
-            self._emitter.emit(f"addq    ${allocated}, %rsp")
 
 
 class Translator(StmtVisitor):
     def __init__(
             self, convention: "CallingConvention", stack: "Stack",
-            writer: "AsmEmitter"):
+            emitter: "AsmEmitter"):
         self._convention = convention
         self._stack = stack
-        self._writer = writer
-        self._expr_translator = ExprTranslator(convention, stack, writer)
+        self._emitter = emitter
+        self._expr_translator = ExprTranslator(convention, stack, emitter)
+
+    def translate(self, stmts: list[Stmt]) -> None:
+        for stmt in stmts:
+            stmt.accept(self)
 
     def visit_assign(self, stmt: Assign) -> None:
         if not self._stack.is_defined(stmt.name):
-            self._writer.emit("subq    $8, %rsp")
-            self._stack.define_var(stmt.name, self._stack.allocate())
+            self._emitter.emit("subq    $8, %rsp")
+            self._stack.define_var(
+                stmt.name, self._stack.allocate(self._emitter))
         stmt.expr.accept(self._expr_translator)
         offset = self._stack.get_offset(stmt.name)
-        self._writer.emit(f"popq    {offset}(%rbp)")
+        self._emitter.emit(f"popq    {offset}(%rbp)")
 
     def visit_return(self, stmt: Return) -> None:
         stmt.expr.accept(self._expr_translator)
-        self._writer.emit("popq    %rax")
-        self._writer.emit("movq    %rbp, %rsp")
-        self._writer.emit("popq    %rbp")
-        self._writer.emit("ret")
+        self._emitter.emit("popq    %rax")
+        self._emitter.emit("movq    %rbp, %rsp")
+        self._emitter.emit("popq    %rbp")
+        self._emitter.emit("ret")
 
     def visit_if(self, stmt: If) -> None:
-        neg = self._writer.get_label()
-        end = self._writer.get_label()
+        neg = self._emitter.get_label()
+        end = self._emitter.get_label()
         stmt.test.accept(self._expr_translator)
-        self._writer.emit("popq    %rax")
-        self._writer.emit("cmpq    $0, %rax")
-        self._writer.emit(f"je      {neg}")
-        self._handle_if_branch(stmt.pos)
-        self._writer.emit(f"jmp     {end}")
-        self._writer.emit(f"{neg}:", indent=False)
-        self._handle_if_branch(stmt.neg)
-        self._writer.emit(f"{end}:", indent=False)
-
-    def _handle_if_branch(self, stmts: list[Stmt]) -> None:
+        self._emitter.emit("popq    %rax")
+        self._emitter.emit("cmpq    $0, %rax")
+        self._emitter.emit(f"je      {neg}")
         stack = self._stack.scope()
-        translator = Translator(self._convention, stack, self._writer)
-        for stmt in stmts:
-            stmt.accept(translator)
-        allocated = stack.allocated()
-        if allocated > 0:
-            self._writer.emit(f"addq    ${allocated}, %rsp")
+        Translator(self._convention, stack, self._emitter).translate(stmt.pos)
+        stack.free(self._emitter)
+        self._emitter.emit(f"jmp     {end}")
+        self._emitter.emit(f"{neg}:", indent=False)
+        stack = self._stack.scope()
+        Translator(self._convention, stack, self._emitter).translate(stmt.neg)
+        stack.free(self._emitter)
+        self._emitter.emit(f"{end}:", indent=False)
 
 
 class Stack:
@@ -243,22 +236,31 @@ class Stack:
             if name in self._offsets or self._next is None \
             else self._next.get_offset(name)
 
-    def allocate(self) -> int:
+    def adjust(self, cc: "CallingConvention", emitter: "AsmEmitter") -> None:
+        offset = 8 * len(cc.registers) if cc.shadow else 0
+        if cc.alignment > 0:
+            offset += cc.alignment + (self._offset - offset) % cc.alignment
+        self._offset -= offset
+        emitter.emit(f"subq    ${offset}, %rsp")
+
+    def allocate(self, emitter: "AsmEmitter") -> int:
         self._offset -= 8
+        emitter.emit("subq    $8, %rsp")
         return self._offset
 
-    def allocated(self) -> int:
-        if self._next is not None:
-            return self._next._offset - self._offset
-        return -self._offset
+    def scope(self) -> "Stack":
+        return Stack(self._offset, self)
+
+    def free(self, emitter: "AsmEmitter") -> None:
+        allocated = -self._offset if self._next is None \
+            else self._next._offset - self._offset
+        if allocated > 0:
+            emitter.emit(f"addq    ${allocated}, %rsp")
 
     def align(self, alignment: int) -> int:
         if alignment > 0:
             return alignment + self._offset % alignment
         return 0
-
-    def scope(self) -> "Stack":
-        return Stack(self._offset, self)
 
 
 @dataclass
